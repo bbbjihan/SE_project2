2c7ab0b8d09561ac4caf56d22b6121760b0eae69
Highdown <37868047+Highdown@users.noreply.github.com>
2023-05-12 01:01:52 +0900

    datacmp 완료

--- main.c
+++ main.c
@@ -58,11 +58,31 @@ void blockcmp(){
 }
 
 //두 개의 파일 수정 날짜를 비교하는 함수 작성
-void datecmp(){
-    
+void datecmp() {
+    printf("date compare\n");
+    if (time1->tm_mon > time2->tm_mon)
+    {
+        printf("text2 is early\n");
+    }
+    else if (time1->tm_mon < time2->tm_mon)
+    {
+        printf("text1 is early\n");
+    }
+    else if (time1->tm_mday > time2->tm_mday)
+    {
+        printf("text2 is early\n");
+    }
+    else if (time1->tm_mday < time2->tm_mday)
+    {
+        printf("text1 is early\n");
+    }
+    else
+    {
+        printf("same date\n");
+    }
 }
 
 //두 개의 파일 수정 시간을 비교하는 함수 작성
-void timecmp(){
+void timecmp() {
     
 }
